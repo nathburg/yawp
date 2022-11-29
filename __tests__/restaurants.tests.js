f9b2406a66df51a9e04cdad77ef7fd6db858cd62
Nathan Burgess <nathans.email.address@gmail.com>
2022-11-29 01:45:29 -0800

    POST /api/v1/restaurants/:id/reviews adds new review for restaurant with id

--- __tests__/restaurants.tests.js
+++ __tests__/restaurants.tests.js
@@ -2,6 +2,23 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...mockUser, ...userProps });
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
 
 describe('user routes', () => {
   beforeEach(() => {
@@ -44,7 +61,24 @@ describe('user routes', () => {
     `);
   });
 
-  test.skip('POST /api/v1/restaurants/id/reviews adds new review for restaurant and is protected by authentication', () => {});
+  test('POST /api/v1/restaurants/:id/reviews adds new review for restaurant and is protected by authentication', async () => {
+    const [agent, user] = await registerAndLogin();
+    const res1 = request(app)
+      .post('/api/v1/restaurants/2/reviews')
+      .send({ userID: user.id, detail: 'Manageable diarrhea' });
+    expect(res1.body).toMatchInlineSnapshot(`undefined`);
+
+    const res2 = await agent
+      .post('/api/v1/restaurants/2/reviews')
+      .send({ userID: user.id, detail: 'Manageable diarrhea' });
+    expect(res2.body).toMatchInlineSnapshot(`
+      Object {
+        "detail": "Manageable diarrhea",
+        "id": "2",
+        "userID": "2",
+      }
+    `);
+  });
 
   test.skip('DELETE /api/v1/reviews/:id deletes reviews if admin or user who posted review', () => {});
 });
