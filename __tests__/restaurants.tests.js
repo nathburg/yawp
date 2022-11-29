e532710b8203ca492f9f79331fc49cb4d0deb15e
Nathan Burgess <nathans.email.address@gmail.com>
2022-11-28 20:29:28 -0800

    GET /api/v1/restaurants returns all restaurants

--- __tests__/restaurants.tests.js
+++ __tests__/restaurants.tests.js
@@ -4,29 +4,32 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('user routes', () => {
-    beforeEach(() => {
-      return setup(pool);
-    });
-    afterAll(() => {
-      pool.end();
-    })
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
 
-    test('GET /api/v1/restaurants shows list of restaurants', () => {
+  test('GET /api/v1/restaurants shows list of restaurants', async () => {
+    const res = await request(app).get('/api/v1/restaurants');
+    expect(res.body).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          "id": "1",
+          "name": "McDonalds",
+        },
+        Object {
+          "id": "2",
+          "name": "Burger King",
+        },
+      ]
+    `);
+  });
 
-    });
+  test.skip('GET /api/v1/restaurants/:id returns detail of restaurant with nested list of reviews', () => {});
 
-    test.skip('GET /api/v1/restaurants/:id returns detail of restaurant with nested list of reviews', () => {
+  test.skip('POST /api/v1/restaurants/id/reviews adds new review for restaurant and is protected by authentication', () => {});
 
-    });
-
-    test.skip('POST /api/v1/restaurants/id/reviews adds new review for restaurant and is protected by authentication', () => {
-
-    });
-
-    test.skip('DELETE /api/v1/reviews/:id deletes reviews if admin or user who posted review', () => {
-
-    });
-
+  test.skip('DELETE /api/v1/reviews/:id deletes reviews if admin or user who posted review', () => {});
 });
-
-
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS restaurants;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,3 +10,12 @@ CREATE TABLE users (
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
 );
+
+CREATE TABLE restaurants (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR NOT NULL
+);
+
+INSERT INTO 
+  restaurants (name)
+VALUES ('McDonalds'), ('Burger King');
